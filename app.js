872a45d4a0545f547aff4a54e9ade83118d9b25b
Gaurav Kumar <Gauravkum1275@gmail.com>
2025-01-01 15:18:08 +0530

    Format Code

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const form = document.querySelector('form');
-form.addEventListener('submit',function(e){
-    e.preventDefault();
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const results = document.querySelector('#results');
-    const bmi = (weight/((height*height)/10000)).toFixed(2);
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+  const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
-    if(height==='' || height<=0 || isNaN(height)){
-      results.innerHTML = `Please enter a valid height `;
-    }
-    else if(weight==='' || weight<=0 || isNaN(weight)){
-        results.innerHTML = `Please enter a valid weight `;
-      }
-    else if(bmi<18.60){
-        results.innerHTML = `The BMI is Under Weight = <span>${bmi}</span>`
-    }
-    else if(bmi>=18.60 && bmi<=24.60){
-        results.innerHTML = `The BMI is Normal Weight = <span>${bmi}</span>`
-    }
-    else{
-           results.innerHTML = `The BMI is Over Weight = <span>${bmi}</span>`
-    }
-    
+  if (height === "" || height <= 0 || isNaN(height)) {
+    results.innerHTML = `Please enter a valid height `;
+  } else if (weight === "" || weight <= 0 || isNaN(weight)) {
+    results.innerHTML = `Please enter a valid weight `;
+  } else if (bmi < 18.6) {
+    results.innerHTML = `The BMI is Under Weight = <span>${bmi}</span>`;
+  } else if (bmi >= 18.6 && bmi <= 24.6) {
+    results.innerHTML = `The BMI is Normal Weight = <span>${bmi}</span>`;
+  } else {
+    results.innerHTML = `The BMI is Over Weight = <span>${bmi}</span>`;
+  }
 });
